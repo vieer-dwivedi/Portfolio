9541a279ed0e1266fb19081e89dbc538658cad62
Vieer dwivedi <vieerdwivedi@gmail.com>
2026-05-14 14:26:11 +0530

    Delete assets directory

--- assets/not-found-8fun_UaZ.js
+++
@@ -1,6 +0,0 @@
-import{c as t,j as e}from"./index-C8PjjpsV.js";import{C as s,a}from"./card-DkLduY9I.js";/**
- * @license lucide-react v0.453.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const r=t("CircleAlert",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]);function x(){return e.jsx("div",{className:"min-h-screen w-full flex items-center justify-center bg-gray-50",children:e.jsx(s,{className:"w-full max-w-md mx-4",children:e.jsxs(a,{className:"pt-6",children:[e.jsxs("div",{className:"flex mb-4 gap-2",children:[e.jsx(r,{className:"h-8 w-8 text-red-500"}),e.jsx("h1",{className:"text-2xl font-bold text-gray-900",children:"404 Page Not Found"})]}),e.jsx("p",{className:"mt-4 text-sm text-gray-600",children:"Did you forget to add the page to the router?"})]})})})}export{x as default};
